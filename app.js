98fbc6b973f13c4ab752519a04fee77fc4f68ea7
bezo16 <bezo16@protonmail.ch>
2021-09-13 14:02:50 +0200

    .

---
+++ app.js
@@ -0,0 +1,22 @@
+ const Tesseract = require('tesseract.js');
+ const fs = require('fs')
+
+ let curent = 1
+ let last = 89
+
+ function transform() {
+   Tesseract.recognize(
+    `./imgs/${curent}.jpg`,
+    'ces',
+    { logger: m => console.log(m) }
+  ).then(({ data: { text } }) => {
+    let logStream = fs.createWriteStream('result.txt', {flags: 'a'});
+    logStream.write( '\n\n\n\n\n'  + text + '\n\n\n\n\n NOVA STRANA NOVA STRANA NOVA STRANA NOVA STRANA');
+    if(curent !== last + 1) {
+      curent++
+      transform()
+     } 
+  })
+ }
+ transform()
+ 
